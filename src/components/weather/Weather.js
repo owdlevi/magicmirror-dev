2cf36cd8b16519cf4adebfd6827373963bca29a9
David Levente Dozsa <dozsalevi@gmail.com>
2019-03-27 08:55:09 +0000

    weather component changes

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactAnimatedWeather from 'react-animated-weather'
+import moment from 'moment'
 import weatherData from '../../utils/weatherFake'
 import { ICON_NAME, iconDefault } from  '../../utils/utils'
 import './Weather.css'
@@ -14,6 +15,7 @@ export default class Weather extends Component {
       color: 'white',
       size: 128,
       summary: weatherData.currently.summary,
+
       temperature: weatherData.currently.temperature
     }
 
@@ -35,34 +37,41 @@ export default class Weather extends Component {
   render() {
     const { today, daily } = this.state.data
 
-    const dailyVal = daily.data.map( dayData => {
-      return <div>
-      <ReactAnimatedWeather
-        icon={ICON_NAME[dayData.icon]}
-        color={iconDefault.color}
-        size={iconDefault.size}
-        animate={false}
-      /></div>
+    const dailyVal = daily.data.slice(0,3).map( dayData => {
+      let day = moment.unix(dayData.time)
+      return <div className='dayWeather'>
+        <span className='dayName'>{day.format('dddd')}</span>
+        <div className='dayIcon'>
+          <ReactAnimatedWeather
+            icon={ICON_NAME[dayData.icon]}
+            color={iconDefault.color}
+            size={iconDefault.size}
+            animate={false}
+          />
+        </div>
+        <div className='dayTemperature'>
+        <span>{Math.round(dayData.temperatureLow)}<sup>&deg;</sup></span>
+        <span>{Math.round(dayData.temperatureHigh)}<sup>&deg;</sup></span>
+        </div>
+      </div>
     })
 
     return <div className='TodaysWeather'>
       <div className='currentWeather'>
         <h3>{today.summary}</h3>
-        <span className='todayTemperature'>{today.temperature}<sup>&deg;</sup></span>
+        <span className='todayTemperature'>{Math.round(today.temperature)}<sup>&deg;</sup></span>
         <ReactAnimatedWeather
-          icon={today.icon}
-          color={today.color}
-          size={today.size}
+          icon  = {today.icon}
+          color = {today.color}
+          size  = {today.size}
         />
-        <div className='maxmin'>
-         <span>3<sup>&deg;</sup></span>
-         <span>16<sup>&deg;</sup></span>
-        </div>
       </div>
 
       <div className='weeklyWeather'>
         <p>{daily.summary}</p>
-        {dailyVal}
+        <div className='dailyList'>
+          {dailyVal}
+        </div>
       </div>
 
       </div>;
