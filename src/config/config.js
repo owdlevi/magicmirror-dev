6e12dcc518e597cb733f5acea7d9c5ec64a5b8b2
David Dozsa <dozsalevi@gmail.com>
2020-02-12 20:27:30 +0000

    change api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 const config = {
-  weatherAPI : 'https://api.magicmirror.site/weather',
-  tflAPI: 'https://api.magicmirror.site/bus',
-  messageAPI: 'https://api.magicmirror.site/messages',
-  tfl:{
-    API_URL: 'https://api.tfl.gov.uk',
-    APP_ID : '148f05be',
-    APP_KEY : '8b44832fbd2a54ed62ab322b92ae26da'
+  weatherAPI: "https://magicmirror.crazycode.tech/weather",
+  tflAPI: "https://magicmirror.crazycode.tech/bus",
+  messageAPI: "https://magicmirror.crazycode.tech/messages",
+  tfl: {
+    API_URL: "https://api.tfl.gov.uk",
+    APP_ID: "148f05be",
+    APP_KEY: "8b44832fbd2a54ed62ab322b92ae26da"
   },
-  busStationID : '490008639S',
-  removeBus: 'E11'
-}
+  busStationID: "490008639S",
+  removeBus: "E11"
+};
 
-export default config
+export default config;
